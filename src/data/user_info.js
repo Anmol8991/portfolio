b9041a39c03bb4bbbb4561a89c1c9192b61e9d9e
Anmol8991 <100134772+Anmol8991@users.noreply.github.com>
2025-08-10 19:08:03 -0400

    Update the profile photo

--- src/data/user_info.js
+++ src/data/user_info.js
@@ -4,7 +4,7 @@ const info = {
     description:
       "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
     role: "Software Engineer",
-    photo: "./Anmol.jpeg",
+    photo: "./Anmol_photo1.jpeg",
     email: "anmol.arora8991@gmail.com",
     tracking_id: "",
   },
